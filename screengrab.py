b7e2a132f49b51c2d9bee7448fd4a22c19ae196e
yashksk <yash2830kaushik@gmail.com>
2024-05-31 14:27:38 +0530

    Canny edge detection

--- screengrab.py
+++ screengrab.py
@@ -2,13 +2,36 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+from . import directkeys
 
-last_time = time.time()
-while(True):
-    screen = np.array(ImageGrab.grab(bbox=(0,40,1000,600)))
-    print('{} seconds'.format(time.time()-last_time))
-    last_time = time.time()
-    cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+for i in list(range(4))[::-1]:
+    print(i+1)
+    time.sleep(1)
+
+print('press')
+directkeys.PressKey(W)
+time.sleep(3)
+print('release')
+directkeys.ReleaseKey(W)
+
+
+def process_img(image):
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Turns the image gray
+    processed_img = cv2.Canny(processed_img, threshold1=100, threshold2=200)
+    # Canny edge detection algorithm
+    return processed_img
+
+# last_time = time.time()
+# while(True):
+#     screen = np.array(ImageGrab.grab(bbox=(0,40,1280,750)))
+#     new_screen = process_img(screen)
+
+#     print('{} seconds'.format(time.time()-last_time))
+#     last_time = time.time()
+#     cv2.imshow('window', new_screen)
+#     #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+#     # Converting color from BGR to RGB
+#     if cv2.waitKey(25) & 0xFF == ord('q'):
+#         cv2.destroyAllWindows()
+#         break
