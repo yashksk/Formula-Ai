2f34a282201c6c66a378880278342c4332c7a132
yashksk <yash2830kaushik@gmail.com>
2024-06-18 00:21:30 +0530

    Hough lines and lane detection

--- screengrab.py
+++ screengrab.py
@@ -3,6 +3,93 @@ from PIL import ImageGrab
 import cv2
 import time
 from directkeys import PressKey, ReleaseKey, W, A, S, D
+from numpy import ones,vstack
+from numpy.linalg import lstsq
+from statistics import mean
+
+def draw_lines(img,lines):
+    try:
+        # finds the maximum y value for a lane marker 
+        # (since we cannot assume the horizon will always be at the same point.)
+        ys = []  
+        for i in lines:
+            for ii in i:
+                ys += [ii[1],ii[3]]
+        min_y = min(ys)
+        max_y = 750
+        new_lines = []
+        line_dict = {}
+
+        for idx,i in enumerate(lines):
+            for xyxy in i:
+                # These four lines:
+                # modified from http://stackoverflow.com/questions/21565994/method-to-return-the-equation-of-a-straight-line-given-two-points
+                # Used to calculate the definition of a line, given two sets of coords.
+                x_coords = (xyxy[0],xyxy[2])
+                y_coords = (xyxy[1],xyxy[3])
+                A = vstack([x_coords,ones(len(x_coords))]).T
+                m, b = lstsq(A, y_coords)[0]
+
+                # Calculating our new, and improved, xs
+                x1 = (min_y-b) / m
+                x2 = (max_y-b) / m
+
+                line_dict[idx] = [m,b,[int(x1), min_y, int(x2), max_y]]
+                new_lines.append([int(x1), min_y, int(x2), max_y])
+
+        final_lanes = {}
+
+        for idx in line_dict:
+            final_lanes_copy = final_lanes.copy()
+            m = line_dict[idx][0]
+            b = line_dict[idx][1]
+            line = line_dict[idx][2]
+            
+            if len(final_lanes) == 0:
+                final_lanes[m] = [ [m,b,line] ]
+                
+            else:
+                found_copy = False
+
+                for other_ms in final_lanes_copy:
+
+                    if not found_copy:
+                        if abs(other_ms*1.1) > abs(m) > abs(other_ms*0.9):
+                            if abs(final_lanes_copy[other_ms][0][1]*1.1) > abs(b) > abs(final_lanes_copy[other_ms][0][1]*0.9):
+                                final_lanes[other_ms].append([m,b,line])
+                                found_copy = True
+                                break
+                        else:
+                            final_lanes[m] = [ [m,b,line] ]
+
+        line_counter = {}
+
+        for lanes in final_lanes:
+            line_counter[lanes] = len(final_lanes[lanes])
+
+        top_lanes = sorted(line_counter.items(), key=lambda item: item[1])[::-1][:2]
+
+        lane1_id = top_lanes[0][0]
+        lane2_id = top_lanes[1][0]
+
+        def average_lane(lane_data):
+            x1s = []
+            y1s = []
+            x2s = []
+            y2s = []
+            for data in lane_data:
+                x1s.append(data[2][0])
+                y1s.append(data[2][1])
+                x2s.append(data[2][2])
+                y2s.append(data[2][3])
+            return int(mean(x1s)), int(mean(y1s)), int(mean(x2s)), int(mean(y2s)) 
+
+        l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
+        l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
+
+        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]
+    except Exception as e:
+        print(str(e))
 
 def regionOfInterest(img, vertices):
     mask = np.zeros_like(img)
@@ -13,18 +100,41 @@ def regionOfInterest(img, vertices):
     return masked
 
 def process_img(image):
+    original_image = image
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Turns the image gray
     processed_img = cv2.Canny(processed_img, threshold1=100, threshold2=200)
     # Canny edge detection algorithm
+    processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
     vertices = np.array([[0,650],[0,380], [500, 310], [780,310], [1280, 380], [1280, 650], [960, 450], [320, 450]])
     processed_img = regionOfInterest(processed_img,[vertices])
-    return processed_img
+    # Feed edges into Hough Lines algo
+    lines = cv2.HoughLinesP(processed_img,1,np.pi/180,180, np.array([]),200,5)
+    try:
+        l1, l2 = draw_lines(original_image,lines)
+        cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 30)
+        cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 30)
+    except Exception as e:
+        print(str(e))
+        pass
+    try:
+        for coords in lines:
+            coords = coords[0]
+            try:
+                cv2.line(processed_img, (coords[0], coords[1]), (coords[2], coords[3]), [255,0,0], 3)
+                
+                
+            except Exception as e:
+                print(str(e))
+    except Exception as e:
+        pass
+
+    return processed_img,original_image
 
 last_time = time.time()
 while(True):
     screen = np.array(ImageGrab.grab(bbox=(0,40,1280,750)))
-    new_screen = process_img(screen)
+    new_screen, original_image = process_img(screen)
     # print('press')
     # PressKey(W)
     # time.sleep(3)
@@ -33,6 +143,7 @@ while(True):
     print('{} seconds'.format(time.time()-last_time))
     last_time = time.time()
     cv2.imshow('window', new_screen)
+    cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
     #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
     # Converting color from BGR to RGB
     if cv2.waitKey(25) & 0xFF == ord('q'):
